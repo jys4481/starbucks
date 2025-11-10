5ae5cd0c632b86ed477696b6e4635f456cd4d124
jys4481 <gudslarhksk@google.com>
2025-11-10 16:15:55 +0900

    user table insert

---
+++ src/main/java/com/jpa/study/starbucks/domain/main/UserSaveMain.java
@@ -0,0 +1,32 @@
+package com.jpa.study.starbucks.domain.main;
+
+import jakarta.persistence.*;
+import com.jpa.study.starbucks.domain.User;
+
+import java.time.LocalDateTime;
+
+public class UserSaveMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabegin");
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+
+            transaction.begin();
+            User user = new User("user@user.com", "user", LocalDateTime.now());
+            entityManager.persist(user);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        emf.close();
+    }
+
+}
