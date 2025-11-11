84e9d5bff3ac9b90e1a3280a1d1daf7674b27b1e
jys4481 <gudslarhksk@google.com>
2025-11-11 14:21:21 +0900

    user 정보 불러오기 UserGetMain

--- src/main/java/com/jpa/study/starbucks/domain/main/UserGetMain.java
+++ src/main/java/com/jpa/study/starbucks/domain/main/UserGetMain.java
@@ -1,36 +1,38 @@
 package com.jpa.study.starbucks.domain.main;
 
-import jakarta.persistence.*;
 import com.jpa.study.starbucks.domain.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
-import java.time.LocalDateTime;
-
-public class UserSaveMain {
+public class UserGetMain {
 
     public static void main(String[] args) {
 
-        // jpabegin Parameter → persistence.xml에서 사용했던 영속단위 이름
-        // EntityManagerFactory DB 연동 기반을 위한 것
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabegin");
         EntityManager entityManager = emf.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
 
         try {
-
             transaction.begin();
-            User user = new User("user@user.com", "user", LocalDateTime.now());
-            // persists → Parmeter로 전달한 Object가 DB에 저장
-            entityManager.persist(user);
+            User user = entityManager.find(User.class, "jys4481@edsk.co.kr");
+            if (user == null) {
+                System.out.println("User 없음");
+            } else {
+                System.out.printf("User 있음 : email=%s, name=%s, createDate=%s\n",
+                        user.getEmail(), user.getName(), user.getCreateDate());
+            }
             transaction.commit();
         } catch (Exception ex) {
             ex.printStackTrace();
             transaction.rollback();
         } finally {
-            // entityManage.close → 커넥션 사용 후 에러유무에 상관없이 닫아주어야 한다.
             entityManager.close();
         }
-        // emf.close → EntityManger도 닫아준다.
+
         emf.close();
+
     }
 
 }
